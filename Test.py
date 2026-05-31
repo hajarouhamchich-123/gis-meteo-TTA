12e4ce20c93debc6f30a5b93fbb58cb4b1cc4faa
hajar <hajarouhamchich@gmail.com>
2026-05-31 16:34:01 +0100

    suppression Test.py

--- Test.py
+++
@@ -1,5 +0,0 @@
-print("Mon projet meteo TTe est pret!")
-import streamlit as st
-
-st.title("Application Météo - Tanger-Tétouan-Al Hoceïma")
-st.write("Bonjour ! L'application fonctionne correctement ✅")
